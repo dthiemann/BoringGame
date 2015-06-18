69f64aa6a2aa87b3c27583099abe2e089fbcfe4b
Dylan Thiemann <dylanthiemann@gmail.com>
2015-06-18 13:37:57 -0600

    Changed my direction, we are now going to build a personal finance manager
    I added a class that handles the transactions

---
+++ BoringGame/BoringGame.Shared/Transaction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoringGame
+{
+    /* 
+     * Enum for determining the type of transaction
+     */
+    public enum Type {
+        Deposit,
+        Withdraw
+    }
+
+    /*
+     * Class that makes up a transaction, whether it is a deposit or a withdraw
+     */ 
+    class Transaction
+    {
+        private float amount;
+        private string description;
+        private Type type; 
+
+        public Transaction(float amount, string description, Type type)
+        {
+            this.amount = amount;
+            if (description != null)
+                this.description = description;
+            else
+                this.description = "No description";
+            this.type = type;
+        }
+
+        /*
+         * Determines if we have a valid transaction (i.e. no negative or 0 values)
+         */
+        public bool CheckIfValid()
+        {
+            if (amount <= 0)
+                return false;
+            return true;
+        }
+    }
+}
--- BoringGame/BoringGame.Windows/Views/MainPage.xaml.cs
+++ BoringGame/BoringGame.Windows/Views/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using BoringGame;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -20,8 +21,11 @@ namespace BoringGame
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+
     public sealed partial class MainPage : Page
     {
+        private ArrayList transactions = new ArrayList();
+
         public MainPage()
         {
             this.InitializeComponent();
